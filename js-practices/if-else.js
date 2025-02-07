a49f1be12a244503382fbb329e3e224c362bdcde
Emirchelo-Lopez <elopezarmen@gmail.com>
2025-02-06 22:31:51 -0600

    Moved if-else exercise to secondary folder and leave out index.js empty

--- js-practices/if-else.js
+++ js-practices/if-else.js
@@ -12,7 +12,45 @@ let age = Number(prompt("Enter your age please:"));
 let height = Number(prompt("Enter your height please:"));
 
 if (age >= 12 && height >= 140) {
-  alert("Your have the required age & height, have fun :D");
+  let hasAHealthCondition = prompt(
+    "Does the visitor have a health condition?"
+  ).toLocaleLowerCase();
+
+  if (hasAHealthCondition === "no") {
+    alert("You have the required age & height, have fun :D");
+  } else {
+    alert("Sorry you must not have a health condition");
+  }
+
+  if (age < 16) {
+    let hasAccompaniment = prompt(
+      "Is the visitor accompanied by an adult?"
+    ).toLocaleLowerCase();
+
+    let hasAHealthCondition = prompt(
+      "Does the visitor have a health condition?"
+    ).toLocaleLowerCase();
+
+    if (hasAccompaniment === "yes" && hasAHealthCondition === "no") {
+      let hasPaid = prompt(
+        "Have you paid your $12USD ticket?:"
+      ).toLocaleLowerCase();
+
+      let hasVIPPass = prompt("Do you have a VIP pass?:").toLocaleLowerCase();
+
+      if (hasPaid === "yes" || hasVIPPass === "yes") {
+        alert("You may pass now");
+      } else {
+        alert("Sorry, you need to pay or have a VIP pass to enter our park");
+      }
+    } else {
+      alert(
+        "Sorry you need to be with an adult AND not having a health condition"
+      );
+    }
+  }
+} else {
+  alert("Sorry you don't have the required age and/or height for this park");
 }
 
 // IF-ELSE EXAMPLE
